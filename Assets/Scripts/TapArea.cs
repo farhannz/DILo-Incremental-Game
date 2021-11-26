75138da21d8ccd47d4e198d78dbc7b85833c090a
Farhan Nurzaman <farhannz@upi.edu>
2021-11-26 23:45:52 +0700

    Initial Commit

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    // Start is called before the first frame update
+    public void OnPointerDown(PointerEventData eventData){
+        GameManager.Instance.CollectByTap(eventData.position,transform);
+    }
+}
